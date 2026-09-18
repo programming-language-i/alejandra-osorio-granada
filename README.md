9bd7f8c8a234bfd1c941bf45f1e0ae98cbad3c3e
alejanddra-osorio-granada <malejita.osorio.g@gmail.com>
2026-09-17 20:14:56 -0500

    Cambios en el archivo calcu.py con operaciones

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Alejandra Osorio Granada
+
 Lenguaje de programacion 1
+
---
+++ calcu.py
@@ -0,0 +1,16 @@
+def suma(valor1,valor2):
+    return valor1 + valor2
+
+def resta(valor1,valor2):
+    return valor1 - valor2
+
+def multi(valor1,valor2):
+    return valor1 * valor2
+
+resultado_suma = suma(5, 2)
+resultado_resta = resta(2, 5)
+resultado_multi = multi(2, 5)
+
+print(f"resultado_suma: {resultado_suma}") 
+print(f"resultado_resta: {resultado_resta}") 
+print(f"resultado_multi: {resultado_multi}") 
