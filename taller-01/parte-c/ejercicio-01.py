0df1d927f44d7cf7c58395a6aeb9a310fbe32f69
alejanddra-osorio-granada <malejita.osorio.g@gmail.com>
2026-09-24 20:54:10 -0500

    Ejercicio 1 sección C

--- taller-01/parte-c/ejercicio-01.py
+++ taller-01/parte-c/ejercicio-01.py
@@ -1,12 +1,19 @@
+#RuntimeError: thread.__init__() not called
+#Se creó un __init__() pero no se inicializó la clase
 import threading
 
 
 class Descarga(threading.Thread):
     def __init__(self, archivo):
+        super().__init__()
         self.archivo = archivo
 
     def run(self):
         print("descargando", self.archivo)
 
-
 Descarga("a.zip").start()
+
+
+
+
+
